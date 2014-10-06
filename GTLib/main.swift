bc6c50fa71f475bb93c0411dec22dd48ca446776
okocsis <okocsis88@gmail.com>
2014-10-06 18:41:47 +0200

    Initial Commit

---
+++ GTLib/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GTLib
+//
+//  Created by Kocsis Olivér on 06/10/14.
+//  Copyright (c) 2014 artit. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
