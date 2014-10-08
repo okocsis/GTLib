16a6464146f5ea68b102566f5ef0e4a8d2871a9a
Kocsis Olivér <okocsis88@gmail.com>
2014-10-08 20:56:00 +0200

    added new iOS target, making it compatible with code 6 and mavericks

--- GTLib/main.swift
+++ GTLib/main.swift
@@ -1,30 +1,33 @@
-var myEnum = MyEnumerator(),
-    mySumm = MySummation(),
-    count = Counting(),
-    myLinS = MyLinSearch(),
-    mySelect = MySelection(),
-    myMaxS = MyMaxSearch()
 
-mySumm.addEnumerator(myEnum)
-count.addEnumerator(myEnum)
-myLinS.addEnumerator(myEnum)
-mySelect.addEnumerator(myEnum)
-myMaxS.addEnumerator(myEnum)
+///this has been moved to appDelegate's testGTlib function
 
-mySumm.run()
-
-count.run()
-
-myLinS.run()
-
-myMaxS.run()
-
-/** WARN: mySelect._elem() holds the CURRENT value of enumerator **/
-mySelect.run()
-
-println("array: \(myEnum.array)")
-println("mySumm: \(mySumm.result())")
-println("count: \(count.result())")
-println("myLinS: found? \(myLinS.found()) \(myLinS._elem())")
-println("mySelect: \(mySelect._elem())")
-println("myMaxS: found? \(myMaxS.found()) \(myMaxS._opt()) \(myMaxS.optElem())")
+//var myEnum = MyEnumerator(),
+//    mySumm = MySummation(),
+//    count = Counting(),
+//    myLinS = MyLinSearch(),
+//    mySelect = MySelection(),
+//    myMaxS = MyMaxSearch()
+//
+//mySumm.addEnumerator(myEnum)
+//count.addEnumerator(myEnum)
+//myLinS.addEnumerator(myEnum)
+//mySelect.addEnumerator(myEnum)
+//myMaxS.addEnumerator(myEnum)
+//
+//mySumm.run()
+//
+//count.run()
+//
+//myLinS.run()
+//
+//myMaxS.run()
+//
+///** WARN: mySelect._elem() holds the CURRENT value of enumerator **/
+//mySelect.run()
+//
+//println("array: \(myEnum.array)")
+//println("mySumm: \(mySumm.result())")
+//println("count: \(count.result())")
+//println("myLinS: found? \(myLinS.found()) \(myLinS._elem())")
+//println("mySelect: \(mySelect._elem())")
+//println("myMaxS: found? \(myMaxS.found()) \(myMaxS._opt()) \(myMaxS.optElem())")
---
+++ GTLib_iOSTests/GTLib_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  GTLib_iOSTests.swift
+//  GTLib_iOSTests
+//
+//  Created by Kocsis Olivér on 2014.10.08..
+//  Copyright (c) 2014 artit. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GTLib_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
